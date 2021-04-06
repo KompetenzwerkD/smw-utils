23127ad83468c4c5999185485eeed38e3b20a381
muehleder <muehleder@saw-leipzig.de>
2021-04-06 15:14:23 +0200

    added doc strings

--- smw_utils/api.py
+++ smw_utils/api.py
@@ -3,19 +3,37 @@ import rdflib
 import urllib
 
 class MediawikiApi:
+"""
+Wrapper class for the Mediawiki API.
+"""
 
-    def __init__(self, url, api, lgname, lgpassword):
+    def __init__(self, url, api, lgname, lgpassword, verbose=False):
+        """
+        Initializes MediawikiApi object.
+
+        :param url: URL of the Mediawiki
+        :param api: URL of the Mediawiki API
+        :param lgname: Username
+        :param lgpassword: Password
+        :param verbose: Display additional information
+        :return: Returns nothing
+        """
         self._url = url
         self._api = api
         self._lgname = lgname
         self._lgpassword = lgpassword
         self._session = requests.Session()
+        self._verbose = verbose
 
         login_token = self._get_login_token()
         self._login(login_token)
 
 
     def _get_login_token(self):
+        """
+        Fetches login token from the Mediawiki API
+        :return: Returns login token
+        """
         token_params = {
             'action':"query",
             'meta':"tokens",
@@ -28,6 +46,10 @@ class MediawikiApi:
 
 
     def _get_csrf_token(self):
+        """
+        Fetches CSRF token from the Mediawiki API.
+        :return: Returns CSRF token
+        """
         token_params = {
             "action": "query",
             "meta": "tokens",
@@ -39,7 +61,11 @@ class MediawikiApi:
 
 
     def _login(self, login_token):
-
+        """
+        Login to mediawiki
+        :param login_token: Mediawiki API login token
+        :return: Returns nothing
+        """
         login_params = {
             "action": "clientlogin",
             "loginreturnurl": self._url,
@@ -49,11 +75,17 @@ class MediawikiApi:
             "format": "json"
         }
         rsp = self._session.post(url=self._api, data=login_params)
-        data = rsp.json()
-        print(data)
+        if self._verbose:
+            print(f"Logged in with user <{self._lgname}>")
 
 
     def fetch_category(self, category):
+        """
+        TODO
+        Fetches all pages in a category.
+        :param category: Category name.
+        :return: List of page titles
+        """
         fetch_params = {
             "action": "query",
             "list": "categorymembers",
@@ -62,15 +94,19 @@ class MediawikiApi:
             "format": "json"
         }
         rsp = self._session.get(url=self._api, params=fetch_params)
-        print(rsp.json())
+        #print(rsp.json())
 
 
-    def _fetch_uri_label(self, g, uri):
-        for s, p, o in g.triples((uri, rdflib.URIRef("http://www.w3.org/2000/01/rdf-schema#label"), None)):
-            print(o)
+    #def _fetch_uri_label(self, g, uri):
+    #    for s, p, o in g.triples((uri, rdflib.URIRef("http://www.w3.org/2000/01/rdf-schema#label"), None)):
+    #        print(o)
         
 
     def fetch_page(self, page):
+        """
+        TODO 
+        Fetches a SMW page and its semantic properties
+        """
         # fetch page text
         fetch_params = {
             "action": "parse",
@@ -91,6 +127,12 @@ class MediawikiApi:
 
 
     def create_page(self, title, content):
+        """
+        Creates a wiki page
+        :param title: Page title
+        :content: Page content
+        :return: Returns nothing
+        """
         csrf_token = self._get_csrf_token()
         payload = {
             "action": "edit",
@@ -100,6 +142,5 @@ class MediawikiApi:
             "text": content
         }
         rsp = self._session.post(url=self._api, data=payload)
-        print(f"... page '{title}' created")
-        #print(rsp.json())
-        
+        if self._verbose:
+            print(f"... page '{title}' created")
